ee05d7d41e1adce0b8d28516087264d10eecf47c
Javier Soto <jisoto2@miuandes.cl>
2020-05-07 10:13:15 -0400

    first commit

---
+++ Ejercicio_evaluado/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio_evaluado
+{
+    [Serializable]
+    public class Persona
+    {
+        public string nombre;
+        public string apellido;
+        public int edad;
+
+        public Persona(string nombre, string apellido , int edad)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad;
+
+        }
+    }
+}
---
+++ Ejercicio_evaluado/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Ejercicio_evaluado
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int eleccion = 0;
+            while(eleccion != 3)
+            {
+                Console.WriteLine("Desea: ");
+                Console.WriteLine("1.- Almacenar Persona");
+                Console.WriteLine("2.- Cargar Persona");
+                Console.WriteLine("3.- Salir");
+                eleccion = Convert.ToInt32(Console.ReadLine());
+
+                if (eleccion == 1 || eleccion == 2)
+                {
+                    IFormatter formatter = new BinaryFormatter();
+                    if (eleccion == 1)
+                    {
+                        Console.WriteLine("Ingrese nombre: ");
+                        string np = Console.ReadLine();
+                        Console.WriteLine("Ingrese apellido: ");
+                        string ap = Console.ReadLine();
+                        Console.WriteLine("Ingrese edad: ");
+                        int ep = Convert.ToInt32(Console.ReadLine());
+                        Persona persona = new Persona(np, ap, ep);
+
+                        Stream stream = new FileStream("MyFile.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+                        formatter.Serialize(stream, persona);
+                        stream.Close();
+
+                    }
+                    else if (eleccion == 2)
+                    {
+                        Stream stream = new FileStream("MyFile.bin", FileMode.Open, FileAccess.Read);
+                        Persona personanew = (Persona)formatter.Deserialize(stream);
+
+                        Console.WriteLine(personanew.nombre + " " + personanew.apellido + " " + personanew.edad);
+
+
+                        
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Opcion invalida.");
+                }
+            }
+           
+        }
+    }
+}
